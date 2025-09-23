63996028e87114ade75bec7da42f0d920a2919d9
Katiyarnitya <137599361+Katiyarnitya@users.noreply.github.com>
2025-09-23 16:41:51 +0530

    Add 3005_CountElementsWithMaxFreq.java solution in Arrays folder

---
+++ Arrays/3005_CountElementsWithMaxFreq.java
@@ -0,0 +1,21 @@
+class Solution {
+   public int maxFrequencyElements(int[] nums) {
+      HashMap<Integer, Integer> map = new HashMap<>();
+      for (int i = 0; i < nums.length; i++) {
+         map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+      }
+
+      int maxFreq = 0;
+      for (int freq : map.values()) {
+         maxFreq = Math.max(maxFreq, freq);
+      }
+
+      int count = 0;
+      for (int freq : map.values()) {
+         if (freq == maxFreq) {
+            count += freq;
+         }
+      }
+      return count;
+   }
+}
