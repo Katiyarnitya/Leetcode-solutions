5fd43885bbf714dc955e149b855f90083c22cd13
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-01-17 16:52:09 +0530

    Time: 10 ms (31.05%), Space: 47.6 MB (58.09%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -9,5 +9,19 @@ class Solution {
             }
         }
         return repEle;
+
+        // for (int i = 0; i < nums.length - 2; i++) {
+        //     if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) {
+        //         return nums[i];
+        //     }
+        // }
+
+
+        //   for (int num : nums) {
+        //     if (!set.add(num)) {
+        //         return num;//return true if element was not added and false if already present in set 
+        //     }
+        // }
+        // return -1;
     }
 }
