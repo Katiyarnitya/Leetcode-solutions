d0187189f1448a4525ed9419eba5d93044121bbe
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-01-18 01:35:25 +0530

    Time: 3 ms (100%), Space: 219.4 MB (0%) - LeetHub

---
+++ 3809-best-reachable-tower/3809-best-reachable-tower.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] bestTower(int[][] towers, int[] center, int radius) {
+        int n = towers.length;
+        int maxQ = -1;
+        int[] ans = {-1,-1};
+        for(int i=0;i<n;i++){
+            int manDis = Math.abs(towers[i][0]-center[0]) + Math.abs(towers[i][1]-center[1]);
+            
+            if(manDis<=radius){//Reachable
+                if(maxQ < towers[i][2]){
+                    maxQ = towers[i][2];
+                    ans[0] = towers[i][0];
+                    ans[1] = towers[i][1];
+                }else if(maxQ == towers[i][2]){ // if quality is equal
+                    if(towers[i][0]<= ans[0] || towers[i][0] == ans[0] && towers[i][1] < ans[1]){
+                        ans[0] = towers[i][0];
+                        ans[1] = towers[i][1];
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
