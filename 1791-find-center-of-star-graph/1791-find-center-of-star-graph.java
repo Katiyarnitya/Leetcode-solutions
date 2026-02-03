1903ebbcb9b0bba5cd8a0a1b9747d98fa59b4541
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-02-03 14:04:03 +0530

    Time: 0 ms (100%), Space: 110.9 MB (59.35%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -20,22 +20,40 @@ class Solution {
         //     }
         // }
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // for(int i=0;i<=V;i++){
+        //     adj.add(new ArrayList<>());
+        // }
+        // for(int i=0;i<e;i++){
+        //     adj.get(edges[i][0]).add(edges[i][1]);
+        //     adj.get(edges[i][1]).add(edges[i][0]);
 
-        for(int i=0;i<=V;i++){
-            adj.add(new ArrayList<>());
-        }
+        // }
+        // for(int i=0;i<=V;i++){
+        //     if(adj.get(i).size()== e){
+        //             return i;
+        //     }
+        // }
 
-        for(int i=0;i<e;i++){
-            adj.get(edges[i][0]).add(edges[i][1]);
-            adj.get(edges[i][1]).add(edges[i][0]);
+        // int[] degree = new int[V+1];
+        // for(int i=0;i<e;i++){
+        //     degree[edges[i][0]]++;
+        //     degree[edges[i][1]]++;
+        // }
+        // for(int i=0;i<V;i++){
+        //     if(degree[i] == e){
+        //         return i;
+        //     }
+        // }
 
+        int a = edges[0][0];
+        int b = edges[0][1];
+        int c = edges[1][0];
+        int d = edges[1][1];
+        if(a==c || a==d){
+            return a;
+        }else{
+            return b;
         }
-        for(int i=0;i<=V;i++){
-            if(adj.get(i).size()== e){
-                    return i;
-            }
-        }
-        return -1;
     }
 }
