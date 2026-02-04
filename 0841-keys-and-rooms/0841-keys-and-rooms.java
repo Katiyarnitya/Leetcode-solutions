b86c121ea169c13890a2edb5e1743cd1c6c9eeb1
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-02-04 12:14:43 +0530

    Time: 2 ms (55.74%), Space: 46.6 MB (11.34%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,12 +1,12 @@
 class Solution {
-    public void BFS( ArrayList<ArrayList<Integer>> adj, boolean[] visited){
+    public void BFS(List<List<Integer>> rooms, boolean[] visited){
         visited[0] = true;
         Queue<Integer> q = new LinkedList<>();
         q.offer(0);
 
         while(!q.isEmpty()){
             int node = q.poll();
-            for(int neigh : adj.get(node)){
+            for(int neigh : rooms.get(node)){
                 if(!visited[neigh]){
                     visited[neigh] = true;
                     q.offer(neigh);
@@ -16,19 +16,17 @@ class Solution {
     }
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // for(int i=0;i<rooms.size();i++){
+        //     adj.add(new ArrayList<>());
+        // }
+        // for(int i=0;i<rooms.size();i++){
+        //    for (int key : rooms.get(i)) {
+        //         adj.get(i).add(key);
+        //     }
+        // }
         boolean[] visited = new boolean[rooms.size()];
-
-        for(int i=0;i<rooms.size();i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<rooms.size();i++){
-           for (int key : rooms.get(i)) {
-                adj.get(i).add(key);
-            }
-        }
-
-        BFS(adj,visited);
+        BFS(rooms,visited);
 
         boolean flag = true;
         for(int i=0;i<visited.length;i++){
