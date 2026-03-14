331a4a31be84864fbcaa0909c827090192f41d2f
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-03-14 10:52:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,8 +3,8 @@ class Solution {
     public boolean solve(int idx, int n, int[] nums){
         if(idx==n-1) return true;
         if(idx>=n) return false;
-
         if(dp[idx] != null) return dp[idx];
+
         boolean result = false;
         for(int i=1;i<=nums[idx]; i++){
             result = result || solve(idx+i,n,nums);
@@ -12,11 +12,20 @@ class Solution {
         return dp[idx] = result;
     }
     public boolean canJump(int[] nums) {
-        
         int n = nums.length;
         if(n==1) return true;
 
-        dp = new Boolean[n];
-        return solve(0,n,nums);
+        // dp = new Boolean[n];
+        // return solve(0,n,nums);
+
+        int maxReachable = nums[0];
+        for(int i=1;i<n;i++){
+
+            if(i>maxReachable) return false;
+            if(i==n-1) return true;
+
+            maxReachable = Math.max(maxReachable, i + nums[i]);
+        }
+        return true;
     }
 }
