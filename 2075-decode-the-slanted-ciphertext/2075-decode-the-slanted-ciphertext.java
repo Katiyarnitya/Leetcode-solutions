e73262b750f3ca7c55f0e61f82d518a416779379
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-04 15:23:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
@@ -7,35 +7,39 @@ class Solution {
         int cols = n/rows;
         char[][] matrix = new char[rows][cols];
 
-        int idx = 0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols; j++){
-                matrix[i][j] = encodedText.charAt(idx);
-                idx++;
-            }
-        }
+        // int idx = 0;
+        // for(int i=0;i<rows;i++){
+        //     for(int j=0;j<cols; j++){
+        //         matrix[i][j] = encodedText.charAt(idx);
+        //         idx++;
+        //     }
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // for(int col=0;col<cols;col++){
+        //     int i = 0;
+        //     int j = col;
+        //     while(i<rows && j<cols){
+        //         sb.append(matrix[i][j]);
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // while (sb.length() > 0 && sb.charAt(sb.length() - 1) == ' ') {
+        //     sb.deleteCharAt(sb.length() - 1);
+        // }
+        // return sb.toString();
 
-        int diagonals =0;
-        for(int i=0;i<n;i++){
-            if(encodedText.charAt(i) == ' '){
-                diagonals = i;
-                break;
-            }
-        }
+        // Aproach 2;
+        StringBuilder originalText = new StringBuilder();
 
-        StringBuilder sb = new StringBuilder();
-        for(int col=0;col<cols;col++){
-            int i = 0;
-            int j = col;
-            while(i<rows && j<cols){
-                sb.append(matrix[i][j]);
-                i++;
-                j++;
+        for(int col = 0;col<cols;col++){
+            for(int i=col; i<n;i+= cols+1){
+                originalText.append(encodedText.charAt(i));
             }
         }
-        while (sb.length() > 0 && sb.charAt(sb.length() - 1) == ' ') {
-            sb.deleteCharAt(sb.length() - 1);
+        while (originalText.length() > 0 && originalText.charAt(originalText.length() - 1) == ' ') {
+            originalText.deleteCharAt(originalText.length() - 1);
         }
-        return sb.toString();
+        return originalText.toString();
     }
 }
