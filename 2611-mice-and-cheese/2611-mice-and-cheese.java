cd31ffdc07717a9c1fcc6f5fcd27f09b12caa2a8
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-16 07:41:12 +0530

    Time: 48 ms (38.14%), Space: 82.9 MB (53.39%) - LeetHub

--- 2611-mice-and-cheese/2611-mice-and-cheese.java
+++ 2611-mice-and-cheese/2611-mice-and-cheese.java
@@ -8,8 +8,7 @@ class Solution {
             arr[i][0] = reward1[i];
             arr[i][1] = reward2[i];
         }
-        Arrays.sort(arr, (a,b)-> Integer.compare((b[0] - b[1]), (a[0] - a[1])));
-
+        Arrays.sort(arr, (a,b)-> Integer.compare((b[0] - b[1]), (a[0] - a[1])));//mouse1-mouse2 
         int profit = 0;
         for(int i=0;i<k;i++){
             profit += arr[i][0];
