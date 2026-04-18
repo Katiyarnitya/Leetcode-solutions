86fdcbc8e59e8dec0943442f95fb781aee9d33aa
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-18 10:13:34 +0530

    Time: 8 ms (91.28%), Space: 48.7 MB (95.27%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,54 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-        ArrayList<int[]> merged = new ArrayList<>();
         int n = intervals.length;
-        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0], b[0]));
-        int startA = intervals[0][0];
-        int endA = intervals[0][1];
+        ArrayList<int[]> ans = new ArrayList<>();
+        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
 
-        for(int i=1; i<n; i++){
+        int prevStart = intervals[0][0];
+        int prevEnd = intervals[0][1];
 
-            int startB = intervals[i][0];
-            int endB = intervals[i][1];
-
-            if(endA >= startB){
-                endA = Math.max(endA, endB);
+        for(int i=1;i<n;i++){
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+            if(prevEnd>= currStart){
+                prevEnd = Math.max(prevEnd, currEnd);
             }else{
-                merged.add(new int[]{startA,endA});
-                startA = startB;
-                endA = endB;
+                ans.add(new int[]{prevStart, prevEnd});
+                prevStart = currStart;
+                prevEnd = currEnd;
             }
         }
-        merged.add(new int[]{startA, endA});
-        return merged.toArray(new int[merged.size()][]);
+        ans.add(new int[]{prevStart,prevEnd});
+
+        return ans.toArray(new int[ans.size()][]);
+
+        
     }
 }
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+        
+//         ArrayList<int[]> merged = new ArrayList<>();
+//         int n = intervals.length;
+//         Arrays.sort(intervals,(a,b) -> Integer.compare(a[0], b[0]));
+//         int startA = intervals[0][0];
+//         int endA = intervals[0][1];
+
+//         for(int i=1; i<n; i++){
+
+//             int startB = intervals[i][0];
+//             int endB = intervals[i][1];
+
+//             if(endA >= startB){
+//                 endA = Math.max(endA, endB);
+//             }else{
+//                 merged.add(new int[]{startA,endA});
+//                 startA = startB;
+//                 endA = endB;
+//             }
+//         }
+//         merged.add(new int[]{startA, endA});
+//         return merged.toArray(new int[merged.size()][]);
+//     }
+// }
