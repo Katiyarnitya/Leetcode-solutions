9d73467516f8aa8b4fadf876b9dfdaef33f76368
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-18 08:19:35 +0530

    Time: 2 ms (39.41%), Space: 94.8 MB (6.18%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,32 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-          int buyPrice=prices[0];
+        
+        int n = prices.length;
 
-          for(int i=1;i<prices.length;i++){
-            if(buyPrice>prices[i]){
-                buyPrice=prices[i];
-            }else{
-                maxProfit = Math.max(maxProfit,prices[i]-buyPrice);
-            }
-          }
-          return maxProfit;
+        int maxProfit = 0;
+        int buyPrice = prices[0];
+
+        for(int i=1;i<n;i++){
+            buyPrice = Math.min(buyPrice,prices[i]);
+
+            maxProfit = Math.max(maxProfit, prices[i]-buyPrice);
+        }
+        return maxProfit;
     }
 }
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int maxProfit = 0;
+//           int buyPrice=prices[0];
+
+//           for(int i=1;i<prices.length;i++){
+//             if(buyPrice>prices[i]){
+//                 buyPrice=prices[i];
+//             }else{
+//                 maxProfit = Math.max(maxProfit,prices[i]-buyPrice);
+//             }
+//           }
+//           return maxProfit;
+//     }
+// }
