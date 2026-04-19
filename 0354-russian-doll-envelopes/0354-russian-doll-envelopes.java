b903161ed449b284d3fd095d31c930e9c84f33cf
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-19 20:36:17 +0530

    Time: 44 ms (48.96%), Space: 101.1 MB (48.34%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -34,36 +34,45 @@ class Solution {
 
     }
     public int maxEnvelopes(int[][] envelopes) {
-         int n = envelopes.length;
-
-        // Step 1: sort (both ascending)
-        Arrays.sort(envelopes, (a,b) -> {
-            if(a[0] == b[0]){
-                return a[1] - b[1];
-            }
-            return a[0] - b[0];
+        Arrays.sort(envelopes,(a,b)->{
+            if(a[0] != b[0]) return a[0]-b[0];
+            return b[1] - a[1]; // height wise descending
         });
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
+        int n = envelopes.length;
+        int[] height = new int[n];
 
-        int max = 1;
-
-        // Step 2: DP
-        for(int i = 0; i < n; i++){
-
-            for(int j = 0; j < i; j++){
-
-                if(envelopes[j][0] < envelopes[i][0] &&
-                   envelopes[j][1] < envelopes[i][1]){
-
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-
-            max = Math.max(max, dp[i]);
+        for(int i = 0;i<n;i++){
+            height[i] = envelopes[i][1];
         }
+        return lengthOfLIS(height);
 
-        return max;
+        // This can be done in opposite fashion also where we will sort on the basis of height and of height is same sort width in descending order
+        // Then apply lis on width
     }
 }
+
+// This can also be the solution but it will give TLE
+
+//  int n = envelopes.length;
+//         Arrays.sort(envelopes, (a,b) -> {
+//             if(a[0] == b[0]){
+//                 return a[1] - b[1];
+//             }
+//             return a[0] - b[0];
+//         });
+
+//         int[] dp = new int[n];
+//         Arrays.fill(dp, 1);
+
+//         int max = 1;
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < i; j++){
+//                 if(envelopes[j][0] < envelopes[i][0] &&
+//                    envelopes[j][1] < envelopes[i][1]){
+//                     dp[i] = Math.max(dp[i], dp[j] + 1);
+//                 }
+//             }
+//             max = Math.max(max, dp[i]);
+//         }
+//         return max;
