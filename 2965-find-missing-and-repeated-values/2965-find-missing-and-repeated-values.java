d238cae58c6bad948c50b7eebf1df18b01589883
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-25 01:25:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,24 +1,45 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         
-        int n = grid.length;
+        int len = grid.length;
+        int n = len*len;
 
-        int[] freq = new int[(n*n)+1];
+        long actualSum = (long)(n*(n+1))/2;
+        long actualSqSum = (long)(n*(n+1)*(2*n+1))/6;
 
-        for(int i=0;i<n;i++){
-            for(int j=0; j<n; j++){
-                freq[grid[i][j]]++;
+        long gridSum = 0;
+        long gridSqSum = 0;
+        for(int i=0;i<len;i++){
+            for(int j=0; j<len; j++){
+                gridSum+= grid[i][j];
+                gridSqSum += (long)(grid[i][j]*grid[i][j]);
             }
         }
-        int[] arr = new int[2];
-        for(int i=1;i<=n*n;i++){
-            if(freq[i] == 2){
-                arr[0] = i;
-            }
-            if(freq[i]==0){
-                arr[1] = i;
-            }
-        }
-        return arr;
+        long diff = gridSum - actualSum;       // a - b
+        long sqDiff = gridSqSum - actualSqSum; // a^2 - b^2
+
+        long sumAB = sqDiff / diff; // a + b
+
+        int a = (int)((diff + sumAB) / 2); // repeating
+        int b = (int)(sumAB - a);          // missing
+
+        return new int[]{a, b};
+        
+        // int[] freq = new int[(n*n)+1];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0; j<n; j++){
+        //         freq[grid[i][j]]++;
+        //     }
+        // }
+        // int[] arr = new int[2];
+        // for(int i=1;i<=n*n;i++){
+        //     if(freq[i] == 2){
+        //         arr[0] = i;
+        //     }
+        //     if(freq[i]==0){
+        //         arr[1] = i;
+        //     }
+        // }
+        // return arr;
     }
 }
