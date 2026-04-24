90ea6cac51c4b1cbaa56ffb687f4f951b1f539b3
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-25 01:49:22 +0530

    Time: 14 ms (42.51%), Space: 53.3 MB (11.69%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,47 +1,69 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        
         int n = nums.length;
 
-        int count1 = 0;
-        int maj1 = -1;
-
-        int count2 = 0;
-        int maj2 = -1;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=0;i<n;i++){
-            if(maj1==nums[i]){
-                count1++;
-            }else if(maj2 == nums[i]){
-                count2++;
-            }else if(count1 == 0){
-                maj1 = nums[i];
-                count1 = 1;
-            }else if(count2==0){
-                maj2 = nums[i];
-                count2 = 1;
-            }else{
-                count1--;
-                count2--;
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        List<Integer> list = new ArrayList<>();
+        int count = n/3;
+
+        for(int key : map.keySet()){
+            if(map.get(key)>count){
+                list.add(key);
             }
         }
-
-        List<Integer> result = new ArrayList<>();
-
-        int freq1 = 0;
-        int freq2 = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i] == maj1){
-                freq1++;
-            }else if(nums[i] == maj2){
-                freq2++;
-            }
-        }
-        if(freq1 > n/3){
-            result.add(maj1);
-        }
-        if(freq2>n/3){
-            result.add(maj2);
-        }
-        return result;
+        return list;
     }
 }
+
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         int n = nums.length;
+
+//         int count1 = 0;
+//         int maj1 = -1;
+
+//         int count2 = 0;
+//         int maj2 = -1;
+
+//         for(int i=0;i<n;i++){
+//             if(maj1==nums[i]){
+//                 count1++;
+//             }else if(maj2 == nums[i]){
+//                 count2++;
+//             }else if(count1 == 0){
+//                 maj1 = nums[i];
+//                 count1 = 1;
+//             }else if(count2==0){
+//                 maj2 = nums[i];
+//                 count2 = 1;
+//             }else{
+//                 count1--;
+//                 count2--;
+//             }
+//         }
+
+//         List<Integer> result = new ArrayList<>();
+
+//         int freq1 = 0;
+//         int freq2 = 0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i] == maj1){
+//                 freq1++;
+//             }else if(nums[i] == maj2){
+//                 freq2++;
+//             }
+//         }
+//         if(freq1 > n/3){
+//             result.add(maj1);
+//         }
+//         if(freq2>n/3){
+//             result.add(maj2);
+//         }
+//         return result;
+//     }
+// }
