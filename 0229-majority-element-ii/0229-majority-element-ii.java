24f1c1c1af73d4d66b98ab93b04a6eefed6c1802
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-25 02:45:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,22 +1,62 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         
+        
         int n = nums.length;
+        int count1 = 0;
+        int maj1 = -1;
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int count2 = 0;
+        int maj2 = -1;
 
         for(int i=0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        List<Integer> list = new ArrayList<>();
-        int count = n/3;
-
-        for(int key : map.keySet()){
-            if(map.get(key)>count){
-                list.add(key);
+            if(maj1==nums[i]){
+                count1++;
+            }else if(maj2 == nums[i]){
+                count2++;
+            }else if(count1 == 0){
+                maj1 = nums[i];
+                count1 = 1;
+            }else if(count2==0){
+                maj2 = nums[i];
+                count2 = 1;
+            }else{
+                count1--;
+                count2--;
             }
         }
-        return list;
+
+        List<Integer> result = new ArrayList<>();
+
+        int freq1 = 0;
+        int freq2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == maj1){
+                freq1++;
+            }else if(nums[i] == maj2){
+                freq2++;
+            }
+        }
+        if(freq1 > n/3){
+            result.add(maj1);
+        }
+        if(freq2>n/3){
+            result.add(maj2);
+        }
+        return result;
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+        // List<Integer> list = new ArrayList<>();
+        // int count = n/3;
+        // for(int key : map.keySet()){
+        //     if(map.get(key)>count){
+        //         list.add(key);
+        //     }
+        // }
+        // return list;
     }
 }
 
@@ -24,46 +64,6 @@ class Solution {
 //     public List<Integer> majorityElement(int[] nums) {
 //         int n = nums.length;
 
-//         int count1 = 0;
-//         int maj1 = -1;
-
-//         int count2 = 0;
-//         int maj2 = -1;
-
-//         for(int i=0;i<n;i++){
-//             if(maj1==nums[i]){
-//                 count1++;
-//             }else if(maj2 == nums[i]){
-//                 count2++;
-//             }else if(count1 == 0){
-//                 maj1 = nums[i];
-//                 count1 = 1;
-//             }else if(count2==0){
-//                 maj2 = nums[i];
-//                 count2 = 1;
-//             }else{
-//                 count1--;
-//                 count2--;
-//             }
-//         }
-
-//         List<Integer> result = new ArrayList<>();
-
-//         int freq1 = 0;
-//         int freq2 = 0;
-//         for(int i=0;i<n;i++){
-//             if(nums[i] == maj1){
-//                 freq1++;
-//             }else if(nums[i] == maj2){
-//                 freq2++;
-//             }
-//         }
-//         if(freq1 > n/3){
-//             result.add(maj1);
-//         }
-//         if(freq2>n/3){
-//             result.add(maj2);
-//         }
-//         return result;
+//         
 //     }
 // }
