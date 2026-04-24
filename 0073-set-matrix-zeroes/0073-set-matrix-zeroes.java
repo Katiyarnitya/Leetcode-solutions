020dfd94cabfd8184d34e80144b666e907d95d20
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-24 23:35:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -4,24 +4,60 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
-        
+        // checking 1st row impacted or not
+
+        boolean firstRowImp = false;
+
+        for(int i=0;i<m; i++){
+            if(matrix[i][0]==0) firstRowImp = true;
+        }
+
+        boolean firstColImp = false;
+        for(int j = 0;j<n;j++){
+            if(matrix[0][j]==0) firstColImp = true;
+        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j] == 0){
-                    row[i] = true;
-                    col[j] = true;
+                    matrix[i][0] = 0; // storing that ith row will be all 0 at it's 1st cell
+                    matrix[0][j] = 0;// storing that jth col will be all 0 at it's 1st cell
                 }
             }
         }
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(row[i] || col[j]){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0 ){
                     matrix[i][j] = 0;
                 }
             }
         }
+        if(firstRowImp){
+            for(int i=0;i<m;i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(firstColImp){
+            for(int j=0;j<n;j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        // boolean[] row = new boolean[m];
+        // boolean[] col = new boolean[n];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j] == 0){
+        //             row[i] = true;
+        //             col[j] = true;
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(row[i] || col[j]){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
     }
 }
