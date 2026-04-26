ca57e7a2820649a985ef716c8fb02e8d184e3196
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-04-27 01:13:50 +0530

    Time: 1 ms (30.46%), Space: 42.5 MB (92.73%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,62 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        
+        int n = s.length();
+        Stack<Integer> openBracSt = new Stack<>(); // storing open indices
+        Stack<Integer> astricSt = new Stack<>(); // storing astric indices
+
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='('){
+                openBracSt.push(i);
+            }else if(s.charAt(i)=='*'){
+                astricSt.push(i);
+            }else{
+                if(!openBracSt.isEmpty()){
+                    openBracSt.pop();
+                }else if(!astricSt.isEmpty()){
+                    astricSt.pop();
+                }else{
+                        return false;
+                }
+            }
+        }
+        while(!openBracSt.isEmpty() && !astricSt.isEmpty()){
+            if(openBracSt.peek() > astricSt.peek()){ // if the astric came before open bracket then no use
+                return false;
+            }
+            astricSt.pop();
+            openBracSt.pop();
+        }
+        return openBracSt.isEmpty();
+    }
+}
+
+// class Solution {
+//     public boolean checkValidString(String s) {
+        
+//         int n = s.length();
+//         int min = 0;
+//         int max = 0;
+
+//         for(int i =0;i<n;i++){
+//             char c = s.charAt(i);
+//             if(c=='('){
+//                 min+=1;
+//                 max+=1;
+//             }else if(c==')'){
+//                 min-=1;
+//                 max-=1;
+//             }else{
+//                 min-=1;
+//                 max+=1;
+//             }
+//             if(min<0){
+//                 min=0;
+//             }
+//             if(max<0){// First  character is ')', That means it can never have opening
+//                 return false;
+//             }
+//         }
+//         return (min==0);
+//     }
+// }
