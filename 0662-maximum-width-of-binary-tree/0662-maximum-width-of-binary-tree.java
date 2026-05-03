733a02aeb1c4688d29cb01483b8b876de25bc8d8
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-05-03 19:26:36 +0530

    Time: 1 ms (99.55%), Space: 45 MB (78.79%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -1,97 +1,63 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    class Pair{
+class Pair {
     TreeNode node;
-    int idx;
-    Pair(TreeNode node, int idx){
+    long idx;
+
+    Pair(TreeNode node, long idx) {
         this.node = node;
         this.idx = idx;
     }
 }
+
+class Solution {
+
     public int widthOfBinaryTree(TreeNode root) {
+
+        if(root == null) return 0;
+
+        Queue<Pair> q = new LinkedList<>();
+
+        q.offer(new Pair(root, 0));
+
         int maxWidth = 0;
-        Deque<Pair> q = new ArrayDeque<>();
-        q.offer(new Pair(root,0));
 
-        while(!q.isEmpty()){
+        while(!q.isEmpty()) {
+
             int size = q.size();
-            int firstIdx = q.peekFirst().idx;
-            int lastIdx = q.peekLast().idx;
 
-            maxWidth = Math.max(maxWidth, lastIdx-firstIdx +1);
+            long minIdx = 0;
+            long lastIdx = 0;
 
-            for(int i=0;i<size;i++){
-                Pair curr = q.poll();
-                TreeNode node = curr.node;
-                int idx = curr.idx;
-                
-                if(node.left!=null){
-                    q.offer(new Pair(node.left, 2*idx+1));
+            for(int i = 0; i < size; i++) {
+
+                Pair p = q.poll();
+
+                TreeNode node = p.node;
+                long idx = p.idx;
+
+                // first node of level
+                if(i == 0) {
+                    minIdx = idx;
                 }
-                if(node.right!=null){
-                    q.offer(new Pair(node.right, 2*idx+2));
+
+                // last node of level
+                if(i == size - 1) {
+                    lastIdx = idx;
+                }
+
+                if(node.left != null) {
+                    q.offer(new Pair(node.left, 2 * idx + 1));
+                }
+
+                if(node.right != null) {
+                    q.offer(new Pair(node.right, 2 * idx + 2));
                 }
             }
+
+            int width = (int)(lastIdx - minIdx + 1);
+
+            maxWidth = Math.max(maxWidth, width);
         }
+
         return maxWidth;
     }
 }
-
-// class Pair{
-//     TreeNode node;
-//     int idx;
-//     Pair(TreeNode node, int idx){
-//         this.node = node;
-//         this.idx = idx;
-//     }
-// }
-// class Solution {
-//     public int widthOfBinaryTree(TreeNode root) {
-//         if (root == null) return 0;
-//         int maxWidth = 0;
-//         Queue<Pair> Q = new LinkedList<>();
-//         Q.offer(new Pair(root,0));
-
-//         while(!Q.isEmpty()){
-//             int size = Q.size();
-//             int levelMinIdx = Q.peek().idx;
-//             int first =0;//index of the first node in that level
-//             int last =0;//index of the last node in that level
-
-//             for(int i=0;i<size;i++){
-//                 Pair currPair = Q.poll();
-//                 TreeNode currNode = currPair.node;
-//                 int idx = currPair.idx - levelMinIdx;
-
-//                 if(i==0){
-//                     first = idx;
-//                 }
-//                 if(i==size-1){
-//                     last = idx;
-//                 }
-//                 if(currNode.left!=null){
-//                     Q.offer(new Pair(currNode.left, 2*idx+1));
-//                 }
-//                 if(currNode.right!=null){
-//                     Q.offer(new Pair(currNode.right, 2*idx+2));
-//                 }
-//             }
-//             maxWidth = Math.max(maxWidth, last - first + 1);
-//         }
-//         return maxWidth;
-//     }
-// }
