7659c507b07f3e2dfb446e85d38d941901005859
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-05-09 22:21:52 +0530

    Time: 6 ms (9.49%), Space: 126.7 MB (6.07%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,46 +1,41 @@
 class Solution {
-        private int lowerBound(ArrayList<Integer> list, int target){
-        
-        int left = 0, right = list.size() - 1;
-        int ans = list.size();
-        
-        while(left <= right){
-            int mid = (left + right) / 2;
-            
-            if(list.get(mid) >= target){
-                ans = mid;
-                right = mid - 1;
-            }
-            else{
-                left = mid + 1;
-            }
-        }
-        
-        return ans;
-    }
     public boolean increasingTriplet(int[] nums) {
-        
         int n = nums.length;
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        list.add(nums[0]);
-        
-        for(int i = 1; i < n; i++){
-            
-            int num = nums[i];
-            
-            if(num > list.get(list.size() - 1)){
-                list.add(num);
-            }
-            else{
-                int idx = lowerBound(list, num);
-                list.set(idx, num);
-            }
-            
-            if(list.size() >= 3) return true;
+
+        int[] leftMin = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMin[0] = nums[0];
+        for(int i=1;i<n;i++){
+            leftMin[i] =Math.min(leftMin[i-1], nums[i]);
         }
-        
+
+        rightMax[n-1] = nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightMax[i] =Math.max(rightMax[i+1], nums[i]);
+        }
+
+        for(int i=1;i<n-1;i++){
+            if(leftMin[i-1] < nums[i] &&
+               nums[i] < rightMax[i+1]){
+                return true;
+            }
+        }
+
+        // TLE
+        // int[] dp = new int[n];
+        // Arrays.fill(dp, 1);
+        // for(int curr=0;curr<n;curr++){
+        //     for(int prev=0;prev<curr;prev++){
+        //         if(nums[prev] < nums[curr]){
+        //             dp[curr] = Math.max(dp[curr],1 + dp[prev]);
+        //             if(dp[curr] >= 3){
+        //                 return true;
+        //             }
+        //         }
+        //     }
+        // }
+
         return false;
-    
     }
 }
