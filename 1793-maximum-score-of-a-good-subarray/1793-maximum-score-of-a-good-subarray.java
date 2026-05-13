b8a466813ffa7592ec92216ba2aa5d0b36a1187a
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-05-13 23:29:24 +0530

    Time: 8 ms (87.61%), Space: 83.4 MB (58.87%) - LeetHub

---
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximumScore(int[] nums, int k) {
+        
+        int n = nums.length;
+        int i = k;
+        int j = k;
+        int currMin = nums[k];
+        int res = nums[k];
+
+        while(i>0 || j<n-1){
+
+            int leftVal = (i>0) ? nums[i-1] : 0;
+            int rightVal = (j<n-1) ? nums[j+1] : 0;
+
+            if(leftVal > rightVal){
+                i--;
+                currMin = Math.min(currMin, nums[i]);
+            }else{
+                j++;
+                currMin = Math.min(currMin, nums[j]);
+            }
+
+            res = Math.max(res, currMin*(j-i+1));
+        }
+        return res;
+    }
+}
