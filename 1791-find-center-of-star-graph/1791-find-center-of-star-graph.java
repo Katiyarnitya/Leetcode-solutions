572d9ead902cef7aae2a4e42165edcf9f8354b84
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-05-14 21:20:06 +0530

    Time: 3 ms (21.99%), Space: 122.3 MB (18.25%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,61 +1,20 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        int V = edges.length+1;
-        int e= edges.length;
 
-        // int[][] matrix = new int[V+1][V+1];
-        // for(int i=0;i<e;i++){
-        //     matrix[edges[i][0]][edges[i][1]] = 1;
-        //     matrix[edges[i][1]][edges[i][0]] = 1;
-        // }
-        // for(int i=0;i<=V;i++){
-        //     int count=0;
-        //     for(int j=0;j<=V; j++){
-        //         if(matrix[i][j] ==1){
-        //             count++;
-        //         }
-        //     }
-        //     if(count==e){
-        //         return i;
-        //     }
-        // }
+        int n = edges.length;
+        int[] deg = new int[n+2];
 
-        // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        // for(int i=0;i<=V;i++){
-        //     adj.add(new ArrayList<>());
-        // }
-        // for(int i=0;i<e;i++){
-        //     adj.get(edges[i][0]).add(edges[i][1]);
-        //     adj.get(edges[i][1]).add(edges[i][0]);
-
-        // }
-        // for(int i=0;i<=V;i++){
-        //     if(adj.get(i).size()== e){
-        //             return i;
-        //     }
-        // }
-
-        int[] degree = new int[V+1];
-        for(int i=0;i<e;i++){
-            degree[edges[i][0]]++;
-            degree[edges[i][1]]++;
+        for(int i=0;i<n;i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            deg[u]++;
+            deg[v]++;
         }
-        for(int i=0;i<=V;i++){
-            if(degree[i] == e){
+        for(int i=1;i<=n+1;i++){
+            if(deg[i]==n){
                 return i;
             }
         }
-
-
-        // int a = edges[0][0];
-        // int b = edges[0][1];
-        // int c = edges[1][0];
-        // int d = edges[1][1];
-        // if(a==c || a==d){
-        //     return a;
-        // }else{
-        //     return b;
-        // }
         return -1;
     }
 }
