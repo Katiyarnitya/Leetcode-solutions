f494697621a61161f2c91ded7e503c1597cac7c5
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-05-16 15:39:43 +0530

    Time: 1 ms (71.8%), Space: 43.3 MB (95.64%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,45 +15,25 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root==null) return ans;
+        
+        if(root==null) return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
 
         while(!q.isEmpty()){
             int size = q.size();
             for(int i=0;i<size;i++){
-                TreeNode node = q.poll();
-                if(node!=null){
-                    if(i==size-1){
-                        ans.add(node.val);
-                    }
-                    if(node.left!=null) q.offer(node.left);
-                    if(node.right!=null) q.offer(node.right);
+                TreeNode curr = q.poll();
+                if(i==size-1){
+                    result.add(curr.val);
                 }
+
+                if(curr.left!=null) q.offer(curr.left);
+                if(curr.right!=null)q.offer(curr.right);
             }
         }
-        return ans;
+        return result;
     }
 }
-
-
-// class Solution {
-//     public List<Integer> helper(TreeNode root, int level,List<Integer> list){
-//         // We will do preorder in reverse dir. i.e Root-> Right -> Left
-//         if(root==null){
-//             return list;
-//         }
-//         if(level==list.size()){
-//             list.add(root.val);
-//         }
-//         helper(root.right,level+1,list);
-//         helper(root.left,level+1,list);
-//     return list;
-//     }
-//     public List<Integer> rightSideView(TreeNode root) {
-//         List<Integer> ans = new ArrayList<>();
-//         helper(root,0,ans);
-//         return ans;
-//     }
-// }
