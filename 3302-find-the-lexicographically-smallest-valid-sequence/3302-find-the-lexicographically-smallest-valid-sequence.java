56af50c864d817d674385e191fdb249d8675c219
Nitya Katiyar <137599361+Katiyarnitya@users.noreply.github.com>
2026-08-09 02:03:05 -0700

    Time: 35 ms (71.05%), Space: 136.8 MB (89.47%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -1,71 +1,54 @@
 class Solution {
     public int[] validSequence(String word1, String word2) {
-
+        
         int n = word1.length();
         int m = word2.length();
 
-        // suf[j] = index where word2[j...] can start matching exactly
-        int[] suf = new int[m];
+        int[] numberOfCharMathFromRHS = new int[n];
 
-        int i = n - 1;
+        int i = n-1;
+        int j = m-1;
 
-        for (int j = m - 1; j >= 0; j--) {
-            while (i >= 0 && word1.charAt(i) != word2.charAt(j)) {
+        int charMatchedCount = 0;
+        while(i>=0 && j>=0){
+            if(word1.charAt(i) == word2.charAt(j)){
+                charMatchedCount++;
+                numberOfCharMathFromRHS[i] = charMatchedCount;
                 i--;
-            }
-
-            if (i < 0) {
-                suf[j] = -1;
-            } else {
-                suf[j] = i;
+                j--;
+            }else{
+                numberOfCharMathFromRHS[i] = (i==n-1) ? 0 : numberOfCharMathFromRHS[i+1];   
                 i--;
             }
         }
+        while(i>=0){
+            numberOfCharMathFromRHS[i] = numberOfCharMathFromRHS[i+1];
+            i--;
+        }
+        i=0;
+        j=0;
 
-        int[] ans = new int[m];
-
-        int pos = 0;
-        boolean changed = false;
-
-        for (int j = 0; j < m; j++) {
-
-            while (pos < n) {
-
-                // Normal matching
-                if (word1.charAt(pos) == word2.charAt(j)) {
-                    ans[j] = pos;
-                    pos++;
-                    break;
+        int[] seq = new int[m];
+        boolean usedModifyingPower = false;
+        int k = 0;
+        while(i<n && j<m){
+            if(word1.charAt(i) == word2.charAt(j)){
+                seq[k] = i;
+                k++;
+                i++;
+                j++;
+            }else{
+                if(usedModifyingPower==false && i+1<n && numberOfCharMathFromRHS[i+1] >= m-1-j){
+                    seq[k] = i;
+                    usedModifyingPower = true;
+                    k++;
+                    i++;
+                    j++;
+                }else{
+                    i++;
                 }
-
-                // Use our one allowed change
-                if (!changed) {
-
-                    // If this is the last character
-                    if (j == m - 1) {
-                        ans[j] = pos;
-                        pos++;
-                        changed = true;
-                        break;
-                    }
-
-                    // Remaining characters must match exactly
-                    if (suf[j + 1] > pos) {
-                        ans[j] = pos;
-                        pos++;
-                        changed = true;
-                        break;
-                    }
-                }
-
-                pos++;
-            }
-
-            if (pos >= n && ans[j] == 0 && j != 0) {
-                return new int[0];
             }
         }
-
-        return ans;
+        return (k != m) ? new int[]{} : seq;
     }
 }
